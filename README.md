06718c1383de3958cbf6318e41090b4b3cd0d33d
GabeNedden <gabenedden@gmail.com>
2020-08-25 22:35:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cinelect
+Let's pick a movie!
+
+find the working version here: https://cinelect.vercel.app/
---
+++ autocomplete.js
@@ -0,0 +1,66 @@
+//dependant on bulma css
+
+const createAutoComplete = ({
+    //where to render autocomplete
+    root,
+    //how to show individual item
+    renderOption,
+    //what to do when clicked
+    onOptionSelect,
+    //what to backfill into input when clicked
+    inputValue,
+    //how to fetch the data
+    fetchData
+}) => {
+    root.innerHTML = `
+        <label><b>Search</b></label>
+        <input class="input">
+        <div class="dropdown">
+            <div class="dropdown-menu">
+                <div class="dropdown-content results"></div>
+            </div>
+        </div>
+    `;
+
+    const input = root.querySelector('input');
+    const dropdown = root.querySelector(".dropdown");
+    const resultsWrapper = root.querySelector(".results");
+
+    const onInput = async event => {
+        const items = await fetchData(event.target.value);
+        if(!items.length){
+            dropdown.classList.remove('is-active');
+            return;
+        }
+
+        resultsWrapper.innerHTML = ''
+        dropdown.classList.add("is-active");
+
+        for (let item of items) {
+            const option = document.createElement('a');
+
+            option.classList.add("dropdown-item");
+            option.innerHTML = renderOption(item);
+            option.addEventListener('click', () => {
+                dropdown.classList.remove('is-active');
+                input.value = inputValue(item);
+                onOptionSelect(item);
+            });
+
+            resultsWrapper.appendChild(option);
+        }
+        input.addEventListener('click', () => {
+            if(items.length){
+                dropdown.classList.add('is-active');
+            }
+        })
+    };
+
+    input.addEventListener('input', debounce(onInput, 800));
+
+    document.addEventListener('click', event => {
+        if(!root.contains(event.target)){
+            dropdown.classList.remove('is-active');
+        }
+    });
+};
